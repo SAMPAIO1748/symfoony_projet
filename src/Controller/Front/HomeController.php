d6755ba82e3485cdb325061bf984d3b61a46c813
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-20 17:25:25 +0100

    Home Page

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller\Front;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home/", name="home")
      */
-    public function home()
+    public function home(CategoryRepository $categoryRepository)
     {
-        dd('hello');
+        $categories = $categoryRepository->findAll();
+        $id = rand(1, count($categories));
+        $categorie = $categoryRepository->find($id);
+        if ($categorie) {
+            return $this->render('front/home.html.twig', ['categorie' => $categorie]);
+        } else {
+            return $this->redirectToRoute('front_home');
+        }
     }
 }
