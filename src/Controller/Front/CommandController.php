d3618e35204865fef327280fa3009e42adb1d2f5
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-23 15:12:46 +0100

    panier

---
+++ src/Controller/Front/CommandController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommandController extends AbstractController
+{
+    /**
+     * @Route("front/cart/add/{id}", name="add_cart")
+     */
+    public function addCart($id, SessionInterface $sessionInterface)
+    {
+        $cart = $sessionInterface->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $sessionInterface->set('cart', $cart);
+
+        return $this->redirectToRoute('front_show_product', ['id' => $id]);
+    }
+
+    /**
+     * @Route("front/cart/", name="show_cart")
+     */
+    public function showCart(
+        SessionInterface $sessionInterface,
+        ProductRepository $productRepository
+    ) {
+        $cart = $sessionInterface->get('cart', []);
+        $cartWithData = [];
+
+        foreach ($cart as $id => $quantity) {
+            $cartWithData[] = [
+                'product' => $productRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        return $this->render('front/cart.html.twig', ['items' => $cartWithData]);
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="delete_cart")
+     */
+    public function deleteCart($id, SessionInterface $sessionInterface)
+    {
+        $cart = $sessionInterface->get('cart', []);
+
+        if (!empty($cart[$id] && $cart[$id] == 1)) {
+            unset($cart[$id]);
+        } else {
+            $cart[$id]--;
+        }
+
+        $sessionInterface->set('cart', $cart);
+
+        return $this->redirectToRoute('show_cart');
+    }
+}
